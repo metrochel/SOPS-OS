e9f05c38c9a7f94ed7090df3f7bd3cd20c9e7f20
Metrochel <ivan.gulkin2016@yandex.ru>
2025-07-28 22:37:21 +0300

    Реализован `assert.c`

---
+++ src/libc/assert.c
@@ -0,0 +1,14 @@
+/*
+ * assert.c
+ *
+ *
+ */
+
+#include "include/assert.h"
+#include "include/stdio.h"
+#include "include/stdlib.h"
+
+void __assert_fail(const char *assertion, const char *file, unsigned int line, const char *func) {
+    perror("%s:%s:%d: Assertion \"%s\" failed\n", file, func, line, assertion);
+    abort();
+}
--- src/libc/include/assert.h
+++ src/libc/include/assert.h
@@ -9,7 +9,7 @@
 #ifdef NDEBUG
     #define assert(X) (void(0))
 #else
-    #define assert(X) if (!(X)) __assert_fail(#X, __FILE__, __LINE__, __func__);
+    #define assert(X) if (!(X)) __assert_fail(#X, __FILE__, __LINE__, __func__)
 #endif
 
 // В случае провала `assert` выводит сообщение о провале и экстренно завершает программу.
